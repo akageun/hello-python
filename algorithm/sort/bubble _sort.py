789fc1a5e6546ccbbab37395720ab5410cacb336
akageun <akageun@gmail.com>
2018-07-24 23:55:25 +0900

    Add Sort Algorithm  - bubble, selection

---
+++ algorithm/sort/bubble _sort.py
@@ -0,0 +1,27 @@
+def bubble_sort(a_list, order_type):
+    a_list_length = len(a_list)
+
+    for i in range(a_list_length - 1, 0, -1):
+
+        for j in range(i):
+            if sort_order_type(a_list[j], a_list[j + 1], order_type):
+                tmp = a_list[j]
+                a_list[j] = a_list[j + 1]
+                a_list[j + 1] = tmp
+
+    return a_list
+
+
+def sort_order_type(pre_val, next_val, order_type):
+    if order_type == 'DESC':
+        return pre_val < next_val
+
+    elif order_type == 'ASC':
+        return pre_val > next_val
+
+
+if __name__ == "__main__":
+    a_list = list(map(int, input("Enter Numbers:").split()))
+
+    print('ASC : ', bubble_sort(a_list, 'ASC'))
+    print('DESC : ', bubble_sort(a_list, 'DESC'))
---
+++ algorithm/sort/selection_sort.py
@@ -0,0 +1,22 @@
+def selection_sort(a_list):
+    a_list_length = len(a_list)
+
+    for i in range(a_list_length - 1):
+        min_index = i
+
+        for j in range(i + 1, a_list_length):  # 최소값 찾기
+            if a_list[j] < a_list[min_index]:
+                min_index = j
+
+        # 위치 변경
+        tmp = a_list[i]
+        a_list[i] = a_list[min_index]
+        a_list[min_index] = tmp
+
+    return a_list
+
+
+if __name__ == "__main__":
+    a_list = list(map(int, input("Enter Numbers:").split()))
+
+    print('선택 정렬 : ', selection_sort(a_list))
